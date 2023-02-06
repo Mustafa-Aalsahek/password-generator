d265ba60f798f24582d1b8d2fea141998f4e88b5
Mustafa Aal-sahek <mustafa.aal-sahek@Mustafas-MacBook-Air.local>
2023-02-06 23:14:48 +0200

    copyButton & adjestments

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function generatePassword() {
     // Initialize variables
     var password = "";
     var possibleChars = "";
-    var possibleSymbols = "!@#%^&*()_+-=[]{}|;':\"<>,.?/\\";
+    var possibleSymbols = "!@#&*_-.?";
     var length = document.getElementById("length-slider").value;
     
     // Determine which character sets to include
@@ -32,3 +32,27 @@ function generatePassword() {
   function updateLength(value) {
     document.getElementById("length-display").innerHTML = value;
   }
+  $('input[role="submit"]').click(function () {
+    
+    var $t = $(this);
+    var hasClass = $t.hasClass('done ');
+    if (!hasClass){
+      $(this).addClass('done ').attr({
+        value: '',
+      });
+    } else if (hasClass){
+      $t.removeClass().attr({
+        value: 'copy',
+      })
+    }
+  });
+  document.getElementById('submit').addEventListener('click', function() {
+  
+    var text = document.getElementById('password');
+    text.select();
+    document.execCommand('copy');
+    setTimeout(()=>{
+
+      window.location.reload();
+    },2000)
+  })
